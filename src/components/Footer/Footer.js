c7ae8dff07267f089ba7a7aa74430643187fc491
jlushington <jay.lushington@gmail.com>
2019-03-12 21:02:29 -0400

    inital commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Footer extends Component{
+
+    render() {
+        return (
+            <h1>This Is Footer</h1>
+        );
+      }
+}
+
+export default Footer;
